35d1a94ae3446f1aab65704ba1813c885e87604e
zhaozhuns_computer <zdq1@192.168.2.203>
2017-04-25 21:57:21 +0800

    chinese translated

---
+++ cycas_gui_chinese.ts
@@ -0,0 +1,460 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>BCDialog</name>
+    <message>
+        <location filename="dialogs.py" line="114"/>
+        <source>Outlet noting to be done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="118"/>
+        <source>Symmetry noting to be done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="122"/>
+        <source>To be implement?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="95"/>
+        <source>slip wall</source>
+        <translation>滑移壁</translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="97"/>
+        <source>no slip wall</source>
+        <translation>无滑移壁</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigDialog</name>
+    <message>
+        <location filename="dialogs.py" line="75"/>
+        <source>OK</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="78"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main.py" line="50"/>
+        <source>Command Line</source>
+        <translation>命令行</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="55"/>
+        <source>cycas-GUI start...</source>
+        <translation>启动Cycas-GUI</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="61"/>
+        <source>Configuration Panel</source>
+        <translation>控制面板</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="77"/>
+        <source>Exit app</source>
+        <translation>退出程序</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="79"/>
+        <source>Import CGNS Mesh</source>
+        <translation>导入CGNS网格</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="81"/>
+        <source>resize the VTK window</source>
+        <translation>重置vtk窗口</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="84"/>
+        <source>Mesh</source>
+        <translation>网格</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="86"/>
+        <source>Output</source>
+        <translation>输出</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="86"/>
+        <source>configure output directory</source>
+        <translation>配置输出目录</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="88"/>
+        <source>Solver</source>
+        <translation>求解器</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="88"/>
+        <source>solver configuration</source>
+        <translation>配置求解器</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="90"/>
+        <source>Material</source>
+        <translation>材料</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="90"/>
+        <source>material configuration</source>
+        <translation>配置物性材料</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="92"/>
+        <source>Boundary</source>
+        <translation>边界</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="92"/>
+        <source>boundary configuration</source>
+        <translation>配置边界</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="111"/>
+        <source>Panel Selection</source>
+        <translation>选择面板</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="143"/>
+        <source>Message</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="143"/>
+        <source>Are you sure to quit?</source>
+        <translation>确定退出？</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="176"/>
+        <source> Panel</source>
+        <translation>面板</translation>
+    </message>
+</context>
+<context>
+    <name>MaterialConfFrame</name>
+    <message>
+        <location filename="dock_frame.py" line="43"/>
+        <source>Materials</source>
+        <translation>材料</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="47"/>
+        <source>Solid Material</source>
+        <translation>固体材料</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="49"/>
+        <source>Liquid Material</source>
+        <translation>流体材料</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="54"/>
+        <source>Add</source>
+        <translation>添加</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="57"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="72"/>
+        <source>material set: %s</source>
+        <translation type="obsolete">已设置材料: %s</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="89"/>
+        <source>material removed: %s</source>
+        <translation type="obsolete">删除材料: %s</translation>
+    </message>
+</context>
+<context>
+    <name>MaterialDialog</name>
+    <message>
+        <location filename="dialogs.py" line="162"/>
+        <source>Select Material From Database</source>
+        <translation>从数据库中选择材料</translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="179"/>
+        <source>Add</source>
+        <translation>添加</translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="182"/>
+        <source>New</source>
+        <translation>新增</translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="203"/>
+        <source>Material</source>
+        <translation>材料</translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="203"/>
+        <source>Properties</source>
+        <translation>物性</translation>
+    </message>
+    <message>
+        <location filename="dialogs.py" line="252"/>
+        <source>wrote to database</source>
+        <translation>已更新数据库</translation>
+    </message>
+</context>
+<context>
+    <name>MeshConfFrame</name>
+    <message>
+        <location filename="dock_frame.py" line="109"/>
+        <source>Input Config</source>
+        <translation>输入配置</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="121"/>
+        <source>Mesh Quality</source>
+        <translation>网格质量</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="123"/>
+        <source>Check Quality</source>
+        <translation>质量检查</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="125"/>
+        <source>Quality Report</source>
+        <translation>质量报告</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="129"/>
+        <source>Mesh Unit</source>
+        <translation>网格单位</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="140"/>
+        <source>Original
+Mesh Unit</source>
+        <translation>原网格单元</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="145"/>
+        <source>Scale ratio</source>
+        <translation>缩放比例</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="159"/>
+        <source>import cgns mesh</source>
+        <translation>导入CGNS网格</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="163"/>
+        <source>loading mesh %s</source>
+        <translation type="obsolete">加载网格%s</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="111"/>
+        <source>Import Mesh</source>
+        <translation>导入网格</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="131"/>
+        <source>Mesh Size:</source>
+        <translation>网格尺寸</translation>
+    </message>
+</context>
+<context>
+    <name>ModelDialog</name>
+    <message>
+        <location filename="dialogs.py" line="138"/>
+        <source>Edit model parameters</source>
+        <translation>编辑模型参数</translation>
+    </message>
+</context>
+<context>
+    <name>OutputConfFrame</name>
+    <message>
+        <location filename="dock_frame.py" line="522"/>
+        <source>ouput configure...</source>
+        <translation>输出配置</translation>
+    </message>
+</context>
+<context>
+    <name>PartsTreeFrame</name>
+    <message>
+        <location filename="dock_frame.py" line="335"/>
+        <source>Volume Parts</source>
+        <translation type="obsolete">体Part</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="338"/>
+        <source>Boundary Parts</source>
+        <translation type="obsolete">边界Part</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="345"/>
+        <source>Set Boundary Type</source>
+        <translation>设置边界类型</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="356"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="358"/>
+        <source>Boundary Type</source>
+        <translation>边界类型</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="359"/>
+        <source>Wall</source>
+        <translation>固壁</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="360"/>
+        <source>Inlet</source>
+        <translation>入口</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="361"/>
+        <source>Outlet</source>
+        <translation>出口</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="362"/>
+        <source>Symmetric</source>
+        <translation>对称</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="363"/>
+        <source>Periodic</source>
+        <translation>周期</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="426"/>
+        <source>double click to focus on part: %s</source>
+        <translation type="obsolete">双击以聚焦于: %s</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="445"/>
+        <source>WALL</source>
+        <translation>固壁</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="448"/>
+        <source>INLET</source>
+        <translation>入口</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="451"/>
+        <source>OUTLET</source>
+        <translation>出口</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="454"/>
+        <source>SYMMETRY</source>
+        <translation>对称</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="457"/>
+        <source>PERIODIC</source>
+        <translation>周期</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="472"/>
+        <source>Set Inlet Boundary</source>
+        <translation>设置入口边界</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="484"/>
+        <source>%s is set to %s</source>
+        <translation type="obsolete">%s 被设为 %s</translation>
+    </message>
+</context>
+<context>
+    <name>SolverConfFrame</name>
+    <message>
+        <location filename="dock_frame.py" line="200"/>
+        <source>Solver Config</source>
+        <translation>求解器配置</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="205"/>
+        <source>Solver Type:</source>
+        <translation>求解器类型</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="206"/>
+        <source>Pressure Based</source>
+        <translation>基于压力</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="208"/>
+        <source>Density Based</source>
+        <translation>基于密度</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="215"/>
+        <source>Time</source>
+        <translation>时间</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="216"/>
+        <source>Transient</source>
+        <translation>瞬态</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="218"/>
+        <source>Steady</source>
+        <translation>稳态</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="224"/>
+        <source>Gravity Vector</source>
+        <translation>重力矢量</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="233"/>
+        <source>Model Selection</source>
+        <translation>模型选择</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="238"/>
+        <source>Models</source>
+        <translation>模型</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="238"/>
+        <source>Description</source>
+        <translation>描述</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="243"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="282"/>
+        <source>model: %s set parameters: %s</source>
+        <translation type="obsolete">为模型: %s 设置了参数: %s</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="305"/>
+        <source>model activated %s</source>
+        <translation type="obsolete">启用模型: %s</translation>
+    </message>
+    <message>
+        <location filename="dock_frame.py" line="314"/>
+        <source>model deactivated %s</source>
+        <translation type="obsolete">关闭模型: %s</translation>
+    </message>
+</context>
+</TS>
